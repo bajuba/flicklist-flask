8b5198413b9ca629a1fc082788430a512db666f9
John Doe <jmunger@lakelandcollege.edu>
2020-09-22 13:56:13 -0500

    added ability to pull random movies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from random import shuffle
 
 app = Flask(__name__)
 
@@ -12,18 +13,26 @@ def index():
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
-    content += "<li>" + movie + "</li>"
+    content += "<li>" + movie[0] + "</li>"
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
-
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + movie[1] + "</li>"
+    content += "</ul>"
     return content
 
 def get_random_movie():
+    newList = []
     # TODO: make a list with at least 5 movie titles
+    movieList = ["The Big Lebowski","The Matrix","District 9","Equilibrium", "Minions"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    shuffle(movieList)
+    newList.append(movieList[0])
+    newList.append(movieList[1])
+    return newList
 
 
 app.run()
